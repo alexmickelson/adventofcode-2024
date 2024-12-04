36f79e2e084a1533d55f1b50547a7e29a5869716
Alex Mickelson <alexmickelson96@gmail.com>
2024-12-03 21:59:38 -0700

    pre day 4

---
+++ day04/src/lib.rs
@@ -0,0 +1,10 @@
+use std::fs;
+
+pub fn read_file_from_path(file_path: &str) -> String {
+    let data = fs::read_to_string(file_path).expect("Unable to read file");
+    println!("{}", data);
+    data
+}
+
+#[cfg(test)]
+mod tests;
